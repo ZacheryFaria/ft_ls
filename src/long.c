a76cbb408f9dd62a9a131df4b8a0cd8c69324260
arwn <windham.aren@protonmail.com>
2019-02-20 18:32:06 -0800

    functab

--- src/long.c
+++ src/long.c
@@ -6,32 +6,41 @@
 /*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 13:01:15 by awindham          #+#    #+#             */
-/*   Updated: 2019/02/20 18:22:14 by awindham         ###   ########.fr       */
+/*   Updated: 2019/02/20 18:31:38 by awindham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <ft_ls.h>
 
+/* 
+** Unix file types,
+** permissions,
+** number of hard links,
+** owner,
+** group,
+** size,
+** last-modified date
+*/
+void (*f[6])(const char *) =
+{
+	long_permissions,
+	long_hardlinks,
+	long_owner,
+	long_group,
+	long_size,
+	long_lastmod
+};
+
 void	longflag(char *path)
 {
-	/* Unix file types, */
-	long_filetypes(path);
-	/* permissions, */
-	long_permissions(path);
-	ft_putstr("  ");
-	/* number of hard links, */
-	long_hardlinks(path);
-	ft_putstr(" ");
-	/* owner, */
-	long_owner(path);
-	ft_putstr(" ");
-	/* group, */
-	long_group(path);
-	ft_putstr(" ");
-	/* size, */
-	long_size(path);
-	ft_putstr(" ");
-	/* last-modified date, */
-	long_lastmod(path);
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		(*f[i])(path);
+		ft_putchar(' ');
+		i++;
+	}
 }
--- src/long_permissions.c
+++ src/long_permissions.c
@@ -6,7 +6,7 @@
 /*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 13:16:00 by awindham          #+#    #+#             */
-/*   Updated: 2019/02/20 18:19:14 by awindham         ###   ########.fr       */
+/*   Updated: 2019/02/20 18:31:57 by awindham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <libft.h>
+#include <ft_ls.h>
 
 void	long_permissions(const char *path)
 {
@@ -21,6 +22,7 @@ void	long_permissions(const char *path)
 
 	if (stat(path, &st) < 0)
 		exit(-1);
+	long_filetypes(path);
 	ft_putchar((st.st_mode & S_IRUSR) ? 'r' : '-');
 	ft_putchar((st.st_mode & S_IWUSR) ? 'w' : '-');
 	ft_putchar((st.st_mode & S_IXUSR) ? 'x' : '-');
@@ -30,4 +32,5 @@ void	long_permissions(const char *path)
 	ft_putchar((st.st_mode & S_IROTH) ? 'r' : '-');
 	ft_putchar((st.st_mode & S_IWOTH) ? 'w' : '-');
 	ft_putchar((st.st_mode & S_IXOTH) ? 'x' : '-');
+	ft_putchar(' ');
 }
