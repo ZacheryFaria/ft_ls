72e58ea4638f0932435622becc8f40877fed4c32
arwn <windham.aren@protonmail.com>
2019-02-22 10:59:27 -0800

    test

--- src/long_getmaxwidth.c
+++ src/long_getmaxwidth.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   long_getmaxwidth.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
+/*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 15:48:46 by awindham          #+#    #+#             */
-/*   Updated: 2019/02/22 10:24:14 by zfaria           ###   ########.fr       */
+/*   Updated: 2019/02/22 10:59:11 by awindham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	largest(char ***s, int i)
 int	*long_getmaxwidth(char ***s)
 {
 	int *done = malloc(7 * sizeof(*done));
-	done[0] = largest(s, 0);
+	done[0] = largest(s, 0) - 1;
 	done[1] = largest(s, 1);
 	done[2] = largest(s, 2);
 	done[3] = largest(s, 3);
