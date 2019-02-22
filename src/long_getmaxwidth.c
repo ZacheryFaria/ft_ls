2353bd11b75bb07fa983a56a53f37e4a9400b439
zacheryfaria <zacheryfaria@gmail.com>
2019-02-22 11:11:56 -0800

    Wowsers mk.2

--- src/long_getmaxwidth.c
+++ src/long_getmaxwidth.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   long_getmaxwidth.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 15:48:46 by awindham          #+#    #+#             */
-/*   Updated: 2019/02/22 10:59:11 by awindham         ###   ########.fr       */
+/*   Updated: 2019/02/22 11:07:08 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ int	*long_getmaxwidth(char ***s)
 {
 	int *done = malloc(7 * sizeof(*done));
 	done[0] = largest(s, 0) - 1;
-	done[1] = largest(s, 1);
+	done[1] = largest(s, 1) + 1;
 	done[2] = largest(s, 2);
-	done[3] = largest(s, 3);
-	done[4] = largest(s, 4);
+	done[3] = largest(s, 3) + 1;
+	done[4] = largest(s, 4) + 1;
 	done[5] = largest(s, 5);
 	done[6] = largest(s, 6);
 	return (done);
