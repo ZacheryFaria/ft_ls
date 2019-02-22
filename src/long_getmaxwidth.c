87ce46e095acba680a0b4774f8b191c719a298f9
arwn <windham.aren@protonmail.com>
2019-02-21 16:16:54 -0800

    allignment

---
+++ src/long_getmaxwidth.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   long_getmaxwidth.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/21 15:48:46 by awindham          #+#    #+#             */
+/*   Updated: 2019/02/21 16:13:32 by awindham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <libft.h>
+
+int	largest(char ***s, int i)
+{
+	int done = 0;
+	int j = 0;
+	
+	while (s[j])
+	{
+		if (done < (int)ft_strlen(s[j][i]))
+			done = (int)ft_strlen(s[j][i]);
+		j++;
+	}
+	return (done + 1);
+}
+
+int	*long_getmaxwidth(char ***s)
+{
+	int *done = malloc(6);
+	done[0] = largest(s, 0);
+	done[1] = largest(s, 1);
+	done[2] = largest(s, 2);
+	done[3] = largest(s, 3);
+	done[4] = largest(s, 4);
+	done[5] = largest(s, 5);
+	done[6] = largest(s, 6);
+s++;
+	return (done);
+}
