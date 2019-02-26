f33e6a9ab5f87af0fc0a695d0c31f5b01b390c28
zacheryfaria <zacheryfaria@gmail.com>
2019-02-26 15:15:32 -0800

    fixed time sort

--- src/time_cmp.c
+++ src/time_cmp.c
@@ -6,7 +6,7 @@
 /*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 12:37:08 by zfaria            #+#    #+#             */
-/*   Updated: 2019/02/21 12:59:11 by zfaria           ###   ########.fr       */
+/*   Updated: 2019/02/26 15:14:51 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,16 @@ int		time_cmp(const char *s1, const char *s2)
 	long		diff;
 	long		diffn;
 
-	stat(s1, &st1);
-	stat(s2, &st2);
+	lstat(s1, &st1);
+	lstat(s2, &st2);
 	diff = st2.st_mtimespec.tv_sec - st1.st_mtimespec.tv_sec;
 	diffn = st2.st_mtimespec.tv_nsec - st1.st_mtimespec.tv_nsec;
 	if (diff == 0)
-		return (diffn);
+	{
+		if (diffn == 0)
+			return ft_strcmp(s1, s2);
+		else
+			return (diffn);	
+	}
 	return (diff);
 }
